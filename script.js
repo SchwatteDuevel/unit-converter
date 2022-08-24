2c792296f15c2b0587430096a0d98141be35783f
SchwatteDuevel <102264881+SchwatteDuevel@users.noreply.github.com>
2022-08-24 23:13:58 +0200

    First Result

---
+++ script.js
@@ -0,0 +1,42 @@
+const input = document.getElementById("input-number");
+const btn = document.getElementById("btn-input");
+const lengthRes = document.getElementById("length-result");
+const volumeRes = document.getElementById("volume-result");
+const massRes = document.getElementById("mass-result");
+
+let meter = "";
+let liter = "";
+let kilogram = "";
+let feet = "";
+let gallon = "";
+let pound = "";
+
+const length = 3.281;
+const volume = 0.264;
+const mass = 2.204;
+
+btn.addEventListener("click", () => {
+    let inputValue = input.value;
+    meter = inputValue / length;
+    feet = inputValue * length;
+    liter = inputValue / volume;
+    gallon = inputValue * volume;
+    kilogram = inputValue / mass;
+    pound = inputValue * mass;
+
+    lengthRes.innerHTML = `${inputValue} meters = ${feet.toFixed(3)} feet | ${inputValue} feet = ${meter.toFixed(3)} meters`;
+    volumeRes.innerHTML = `${inputValue} liters = ${gallon.toFixed(3)} gallons | ${inputValue} gallons = ${liter.toFixed(3)} liters`;
+    massRes.innerHTML = `${inputValue} kilos = ${pound.toFixed(3)} pounds | ${inputValue} pounds = ${kilogram.toFixed(3)} kilos`;
+    input.value = "";
+})
+
+const btnTheme = document.getElementById("switchMode");
+const theme = document.getElementsByTagName("link")[0];
+
+btnTheme.addEventListener("click", () => {
+        if (theme.getAttribute('href') == 'styles.css') {
+            theme.setAttribute('href', 'styles-dark.css');
+        } else {
+            theme.setAttribute('href', 'styles.css');
+        }
+    })
